3d28b3122973ddcac58eec73a237306f4fcaf8a7
Tjalf Stadel <tjalf.stadel@web.de>
2023-06-08 22:17:56 +0200

    black .

--- calc1.py
+++ calc1.py
@@ -2,7 +2,8 @@
 #
 # EOF (end-of-file) token is used to indicate that
 # there is no more input left for lexical analysis
-INTEGER, PLUS, WHITESPACE,EOF = 'INTEGER', 'PLUS', 'WHITESPACE','EOF'
+INTEGER, PLUS, MINUS, WHITESPACE, EOF = "INTEGER", "PLUS", "MINUS", "WHITESPACE", "EOF"
+
 
 class Token:
     def __init__(self, type, value):
@@ -16,7 +17,7 @@ class Token:
             Token(INTEGER, 3)
             Token(PLUS '+')
         """
-        return f'Token({self.type}, {self.value!r})'
+        return f"Token({self.type}, {self.value!r})"
 
     def __repr__(self):
         return self.__str__()
@@ -32,7 +33,7 @@ class Interpreter:
         self.current_token = None
 
     def error(self):
-        raise Exception('Error parsing input')
+        raise Exception("Error parsing input")
 
     def get_next_token(self):
         """Lexical analyzer (also known as scanner or tokenizer)
@@ -42,26 +43,17 @@ class Interpreter:
         """
         text = self.text
 
-        # is self.pos index past the end of the self.text ?
-        # if so, then return EOF token because there is no more
-        # input left to convert into tokens
         if self.pos > len(text) - 1:
             return Token(EOF, None)
 
-        # get a character at the position self.pos and decide
-        # what token to create based on the single character
         current_char = text[self.pos]
 
-        # if the character is a digit then convert it to
-        # integer, create an INTEGER token, increment self.pos
-        # index to point to the next character after the digit,
-        # and return the INTEGER token
         if current_char.isdigit():
             token = Token(INTEGER, int(current_char))
             self.pos += 1
             return token
 
-        if current_char == '+':
+        if current_char == "+":
             token = Token(PLUS, current_char)
             self.pos += 1
             return token
@@ -69,10 +61,6 @@ class Interpreter:
         self.error()
 
     def eat(self, token_type):
-        # compare the current token type with the passed token
-        # type and if they match then "eat" the current token
-        # and assign the next token to the self.current_token,
-        # otherwise raise an exception.
         if self.current_token.type == token_type:
             self.current_token = self.get_next_token()
         else:
@@ -80,27 +68,14 @@ class Interpreter:
 
     def expr(self):
         """expr -> INTEGER PLUS INTEGER"""
-        # set current token to the first token taken from the input
         self.current_token = self.get_next_token()
 
-        # we expect the current token to be a single-digit integer
         left = self.current_token
         self.eat(INTEGER)
-
-        # we expect the current token to be a '+' token
         op = self.current_token
         self.eat(PLUS)
-
-        # we expect the current token to be a single-digit integer
         right = self.current_token
         self.eat(INTEGER)
-        # after the above call the self.current_token is set to
-        # EOF token
-
-        # at this point INTEGER PLUS INTEGER sequence of tokens
-        # has been successfully found and the method can just
-        # return the result of adding two integers, thus
-        # effectively interpreting client input
         result = left.value + right.value
         return result
 
@@ -108,9 +83,7 @@ class Interpreter:
 def main():
     while True:
         try:
-            # To run under Python3 replace 'raw_input' call
-            # with 'input'
-            text = input('calc> ')
+            text = input("calc> ")
         except EOFError:
             break
         if not text:
@@ -120,5 +93,5 @@ def main():
         print(result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
